af3297ac956651f772bff9b498f52f8f7423a61a
Amellalli <amellallimiranda712@gmail.com>
2026-02-06 14:56:09 -0600

    Configuracion de CORS

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors({
     origin: '*', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
